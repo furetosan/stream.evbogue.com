d08cbfc673dfbaa1006c4b679d1a82d51eb2bbb8
Ev Bogue <ev@evbogue.com>
2013-02-17 23:04:49 -0500

    change port

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 5051);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.locals.pretty = true;
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
